cff18516ef7c0b2d33e28bc886c5f7939d33f5af
qayol1 <qayol@freemail.hu>
2017-06-26 14:34:51 +0200

    base browsing function implemented

--- SanityArchiver/SanityArchiver/Form1.Designer.cs
+++ SanityArchiver/SanityArchiver/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FileListView = new System.Windows.Forms.ListView();
+            this.Column0 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Column1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Column2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PathBox = new System.Windows.Forms.TextBox();
+            this.PathLabel = new System.Windows.Forms.Label();
+            this.Column3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FileListView
+            // 
+            this.FileListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Column0,
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.FileListView.Location = new System.Drawing.Point(12, 59);
+            this.FileListView.Name = "FileListView";
+            this.FileListView.Size = new System.Drawing.Size(439, 406);
+            this.FileListView.TabIndex = 1;
+            this.FileListView.UseCompatibleStateImageBehavior = false;
+            this.FileListView.View = System.Windows.Forms.View.Details;
+            this.FileListView.SelectedIndexChanged += new System.EventHandler(this.FileListView_SelectedIndexChanged);
+            // 
+            // Column0
+            // 
+            this.Column0.Text = "Name";
+            this.Column0.Width = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.Text = "Path";
+            this.Column1.Width = 109;
+            // 
+            // Column2
+            // 
+            this.Column2.Text = "Type";
+            this.Column2.Width = 105;
+            // 
+            // PathBox
+            // 
+            this.PathBox.Location = new System.Drawing.Point(129, 33);
+            this.PathBox.Name = "PathBox";
+            this.PathBox.ReadOnly = true;
+            this.PathBox.Size = new System.Drawing.Size(322, 20);
+            this.PathBox.TabIndex = 2;
+            // 
+            // PathLabel
+            // 
+            this.PathLabel.AutoSize = true;
+            this.PathLabel.Location = new System.Drawing.Point(91, 36);
+            this.PathLabel.Name = "PathLabel";
+            this.PathLabel.Size = new System.Drawing.Size(32, 13);
+            this.PathLabel.TabIndex = 3;
+            this.PathLabel.Text = "Path:";
+            // 
+            // Column3
+            // 
+            this.Column3.Text = "Size";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "<--";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PathLabel);
+            this.Controls.Add(this.PathBox);
+            this.Controls.Add(this.FileListView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.ListView FileListView;
+        private System.Windows.Forms.ColumnHeader Column0;
+        private System.Windows.Forms.ColumnHeader Column1;
+        private System.Windows.Forms.ColumnHeader Column2;
+        private System.Windows.Forms.TextBox PathBox;
+        private System.Windows.Forms.Label PathLabel;
+        private System.Windows.Forms.ColumnHeader Column3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SanityArchiver/SanityArchiver/Form1.cs
+++ SanityArchiver/SanityArchiver/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,137 @@ namespace SanityArchiver
 {
     public partial class Form1 : Form
     {
+
+        static DirectoryInfo currentDirectory;
+        static List<FileInfo> FileList;
+        static List<DirectoryInfo> DirList;
+        static String path;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileList = new List<FileInfo>();
+            DirList = new List<DirectoryInfo>();
+            path = "C:/";
+            FillData();
+            ShowData();
+           
+        }
+
+        private void FillData()
+        {
+            currentDirectory = new DirectoryInfo(path);
+            AddFiles();
+            AddDirectories();
+        }
+
+        private void AddDirectories()
+        {
+            DirList.Clear();
+            try
+            {
+                foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+                {
+                    DirList.Add(dir);
+                }
+            } catch (UnauthorizedAccessException)
+            {
+
+            }
+            
+        }
+
+        private void AddFiles()
+        {
+            FileList.Clear();
+            try
+            {
+                foreach (FileInfo fil in currentDirectory.GetFiles())
+                {
+                    FileList.Add(fil);
+                }
+            } catch (UnauthorizedAccessException)
+            {
+                
+            }
+            
+        }
+
+        private void ShowData()
+        {
+            FileListView.Items.Clear();
+            foreach (DirectoryInfo dir in DirList)
+            {
+                ListViewItem item = FileListView.Items.Add(dir.Name);
+                item.SubItems.Add(dir.FullName);
+                item.SubItems.Add("File folder");
+            }
+            foreach (FileInfo file in FileList)
+            {
+                ListViewItem item = FileListView.Items.Add(file.Name);
+                item.SubItems.Add(file.FullName);
+                item.SubItems.Add(file.Extension);
+                item.SubItems.Add(ConvertBytes(file.Length));
+                FileListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                FileListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            }
+            PathBox.Text = currentDirectory.FullName;
+            FileListView.Columns[1].Width = 0;
+        }
+       
+        private string ConvertBytes(double FileLength)
+        {
+            if (FileLength < 1024)
+            {
+                return FileLength.ToString() + " byte";
+            }
+            if (FileLength >= 1024 && FileLength < 1024 * 1024)
+            {
+                return (FileLength / 1024).ToString("n2") + " kB";
+            }
+            if (FileLength >= 1024 * 1024 && FileLength < 1024 * 1024 * 1024)
+            {
+                return (FileLength / (1024 * 1024)).ToString("n2") + " MB";
+            }
+            if (FileLength >= 1024 * 1024 * 1024)
+            {
+                return (FileLength / (1024 * 1024 * 1024)).ToString("n2") + " GB";
+            }
+            return "";
+        }
+
+        private void FileListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (FileListView.FocusedItem.SubItems[2].Text.Equals("File folder"))
+            {
+                path = FileListView.FocusedItem.SubItems[1].Text;
+                GoToNewDir();
+
+            }
+        }
+
+        private void GoToNewDir()
+        {
+            PathBox.Text = path;
+            FillData();
+            ShowData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                currentDirectory = Directory.GetParent(path);
+                path = currentDirectory.FullName;
+                GoToNewDir();
+            } catch (NullReferenceException)
+            {
+
+            }
         }
     }
 }
